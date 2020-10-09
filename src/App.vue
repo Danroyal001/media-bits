5585744644979a8759da190a5488ceeeb6038e42
Daniel Effiong <danroyal001@pop-os.localdomain>
2020-10-09 17:09:49 +0100

    2nd commit

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,15 @@
     <nav class="teal">
       <div class="container">
         <div class="nav-wrapper">
-          <a href="#" class="brand-logo">MEDIA-BITS</a>
+          <a href="#" class="brand-logo">
+            <img src="@/assets/logo.png" style="height: 50px; padding-top: 21px;">
+            MEDIA-BITS
+          </a>
           <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="javascript:void()">HOME</a></li>
             <li><a href="javascript:void()">EDITOR</a></li>
+            <li><a href="javascript:void()">SUBSCRIBTION</a></li>
           </ul>
         </div>
       </div>
@@ -17,6 +21,7 @@
   <ul class="sidenav" id="mobile-demo">
     <li><a href="javascript:void()">HOME</a></li>
     <li><a href="javascript:void()">EDITOR</a></li>
+    <li><a href="javascript:void()">SUBSCRIBTION</a></li>
   </ul>
 
   </div>
