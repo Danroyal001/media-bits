f8daacb4af0bb4e742ec1f41398694ca4700ee85
Daniel Effiong <danroyal001@pop-os.localdomain>
2020-11-12 16:21:14 +0100

    Nov 12th 2020 - 4:21pm

--- src/router/index.js
+++ src/router/index.js
@@ -5,42 +5,15 @@ import NetworkError from '@/components/NetworkError.vue';
 import PreLoader from '@/components/PreLoader.vue';
 
 const onError = (error, retry, fail, attempts) => {
-  if (error.message.match(/fetch/) && attempts <= 3) {
-    // retry on fetch errors, 3 max attempts
+  if (error.message.match(/fetch/) && attempts <= 5) {
     retry()
   } else {
-    // Note that retry/fail are like resolve/reject of a promise:
-    // one of them must be called for the error handling to continue.
     fail()
   }
 }
 const timeout = 120000;
 const delay = 1;
 const suspensible = false;
-
-const getComponent = componentURL => defineAsyncComponent({
-  // The factory function
-  loader: () => import(`${componentURL}`),
-  // A component to use while the async component is loading
-  loadingComponent: PreLoader,
-  // A component to use if the load fails
-  errorComponent: NetworkError,
-  // Delay before showing the loading component. Default: 200ms.
-  delay,
-  // The error component will be displayed if a timeout is
-  // provided and exceeded. Default: 120000.
-  timeout,
-  // Defining if component is suspensible. Default: true.
-  suspensible,
-  /**
-   *
-   * @param {*} error Error message object
-   * @param {*} retry A function that indicating whether the async component should retry when the loader promise rejects
-   * @param {*} fail  End of failure
-   * @param {*} attempts Maximum allowed retries number
-   */
-  onError,
-});
 
 const routes = [
   {
@@ -52,27 +25,67 @@ const routes = [
   {
     path: '/editor',
     name: 'editor',
-    component: getComponent('@/views/Editor.vue')
+    component: defineAsyncComponent({
+      loader: () => import('@/views/Editor.vue'),
+      loadingComponent: PreLoader,
+      errorComponent: NetworkError,
+      delay,
+      timeout,
+      suspensible,
+      onError,
+    })
   },
   {
     path: '/contact',
     name: 'contact',
-    component: getComponent('@/views/ContactPage.vue')
+    component: defineAsyncComponent({
+      loader: () => import('@/views/ContactPage.vue'),
+      loadingComponent: PreLoader,
+      errorComponent: NetworkError,
+      delay,
+      timeout,
+      suspensible,
+      onError,
+    })
   },
   {
     path: '/download',
     name: 'download',
-    component: getComponent('@/views/DownloadPage.vue')
+    component: defineAsyncComponent({
+      loader: () => import('@/views/DownloadPage.vue'),
+      loadingComponent: PreLoader,
+      errorComponent: NetworkError,
+      delay,
+      timeout,
+      suspensible,
+      onError,
+    })
   },
   {
     path: '/docummentation',
     name: 'documentation',
-    component: getComponent('@/views/Documentation.vue')
+    component: defineAsyncComponent({
+      loader: () => import('@/views/Documentation.vue'),
+      loadingComponent: PreLoader,
+      errorComponent: NetworkError,
+      delay,
+      timeout,
+      suspensible,
+      onError,
+    })
   },
   {
     path: '/subscribtion',
     name: 'subscribtion',
-    component: getComponent('@/views/Subscribtion.vue')
+    component: defineAsyncComponent({
+      loader: () => import('@/views/Subscribtion.vue'),
+      loadingComponent: PreLoader,
+      errorComponent: NetworkError,
+      delay,
+      timeout,
+      suspensible,
+      onError,
+    })
   }
 ]
 
