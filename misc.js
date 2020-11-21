2e80be9c6894d3b579b55c983dd33206ec7d2920
danroyal001 <danroyaleffiong@gmail.com>
2020-11-21 17:09:58 +0100

    Mon 23rd Nov 2020 - morning (1)

--- misc.js
+++ misc.js
@@ -64,3 +64,78 @@ $self.onmessage = function(message){
     worker.postMessage(base64);
   });
 // end window.__base64ToArrayBuffer
+
+window.convertProjectToFile = window.electronConvertProjectToFile || function(file){
+  return new Promise(resolve => {
+            file = JSON.stringify(file);
+            //file = (new TextEncoder('utf-8')).encode(file);
+            //file = file.toString();
+            const d = new Date();
+            const day = d.getDate();
+            const month = d.getMonth() + 1;
+            const year = d.getFullYear();
+            const hour = d.getHours();
+            const minutes = d.getMinutes();
+            const seconds = d.getSeconds();
+            const fileName = `${year}-${month + ' november'}-${day}_${hour > 12 ? hour - 12 : hour}-${minutes}-${seconds}${hour > 12 ? 'pm' : 'am'}.mbits`; 
+            const jszip = new window.JSZip();
+            jszip.file(fileName, file);
+            jszip.generateAsync({
+              type:'blob',
+              compressionOptions: {
+                level: 10
+              }
+            }).then(blob => {
+              blob = URL.createObjectURL(blob);
+              resolve({blob, fileName});
+            })
+            // let blob = new Blob([file], {
+              // type: 'text/plain'
+            // });
+  });
+}
+ 
+window.loadFromOpenedFile = window.electronLoadFromOpenedFile || function(file){
+  // document
+  return new Promise((resolve, reject) => {
+    const jszip = new window.JSZip();
+    return jszip.loadAsync(file).then(file2 => {
+        file = file2
+        Object.values(file.files)[0].async('string').then(str => {
+        file = str;
+        try {
+          //file = JSON.parse(`[${file}]`);
+          file = JSON.parse(`${file}`);
+        } catch (exception){
+          return reject(exception);
+        }
+        //file = new Uint8Array(file);
+        //file = (new TextDecoder('utf-8')).decode(file);
+        //file = JSON.parse(file);
+        return resolve(file);
+      })
+    });
+  });
+}
+//refactor
+window.loadFromOpenedFile = window.electronLoadFromOpenedFile || function(file){
+  return new Promise((resolve, reject) => {
+    const jszip = new window.JSZip();
+    return jszip.loadAsync(file).then(file2 => {
+        file = file2
+        // console.log(file)
+        Object.values(file.files)[0].async('string').then(str => {
+        file = str;
+        try {
+          file = JSON.parse(`[${file}]`);
+        } catch (exception){
+          return reject(exception);
+        }
+        file = new Uint8Array(file);
+        file = (new TextDecoder('utf-8')).decode(file);
+        file = JSON.parse(file);
+        return resolve(file);
+      })
+    });
+  });
+}
